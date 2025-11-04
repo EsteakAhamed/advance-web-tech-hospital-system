f75a121327e2e43bbf57509fd1d925552bd2e2b7
Maimuna Tasnim <155242962+khorshida@users.noreply.github.com>
2025-11-05 02:59:24 +0600

    Doctor module,service,controller code added

---
+++ src/doctor/dto/create-doctor.dto.ts
@@ -0,0 +1,8 @@
+export class CreateDoctorDto {
+  name: string;
+  specialization: string;
+  email: string;
+  phone: string;
+  department: string;
+  
+}
---
+++ src/doctor/dto/update-doctor.dto.ts
@@ -0,0 +1,8 @@
+export class UpdateDoctorDto {
+  name?: string;
+  specialization?: string;
+  email?: string;
+  phone?: string;
+  department?: string;
+  
+}
