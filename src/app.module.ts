83d388a933c41b0970aa347e3597327cbc819f14
Maimuna Tasnim <155242962+khorshida@users.noreply.github.com>
2025-11-05 03:05:37 +0600

    Revert "Doctor module,service,controller code added"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DoctorModule } from './doctor/doctor.module';  
 
 @Module({
-  imports: [DoctorModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
